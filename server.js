7bc28102f4f98b6c81ca7b9db1d4b6275aba4f6f
Daniel Miller <104872554+danielmiller18@users.noreply.github.com>
2022-12-03 17:58:54 -0600

    added post method for user input in the back end

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 var PORT = 3000;
 app.use(express.static(__dirname + '/public'))
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.use(express.json());
 
@@ -34,16 +34,39 @@ app.get("/notes", (req, res) => {
 
 app.get("/api/notes", (req, res) => {
     fs.readFile(path.join(__dirname, "./Develop/db/db.json"), "utf8",
-    
-    (error, note) => {
-        if(error){
-            return console.log(error)
-        }
-        res.json(JSON.parse(notes))
-    })
+
+        (error, note) => {
+            if (error) {
+                return console.log(error)
+            }
+            res.json(JSON.parse(notes))
+        })
 })
 
 
 // we will use POST method to bring the user input towards the back end
 
-app
+app.post("/api/notes", (req, res) => {
+    const currentNote = req.body;
+
+
+    fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error.notes) => {
+        if(error) {
+            return console.log(error)
+        }
+    })
+
+    notes = JSON.parse(notes);
+
+
+    // assign unique id to each new note depending onthe last ID when there will be noitems in notes array the we assign the id as a number i.e 10, 20
+    if (notes.length > 0) {
+        let lastId = notes[notes.length - 1].id;
+        var id = parseInt(lastId) + 1;
+    } else {
+        var id = 10
+    }
+
+
+    // create a new note object
+})
